c8be9e95aac644b7fabae4f4fede88f548ac1285
Chronicle20 <a.chronicle.20@gmail.com>
2025-02-09 16:30:20 -0500

    Refactoring of @award item command.

--- services/atlas-messages/atlas.com/messages/character/inventory/commands.go
+++ services/atlas-messages/atlas.com/messages/character/inventory/commands.go
@@ -3,75 +3,87 @@ package inventory
 import (
 	"atlas-messages/character"
 	"atlas-messages/command"
+	_map "atlas-messages/map"
 	"context"
+	"github.com/Chronicle20/atlas-model/model"
 	"github.com/sirupsen/logrus"
 	"regexp"
 	"strconv"
-	"strings"
 )
 
 func AwardItemCommandProducer(l logrus.FieldLogger) func(ctx context.Context) func(worldId byte, channelId byte, c character.Model, m string) (command.Executor, bool) {
 	return func(ctx context.Context) func(worldId byte, channelId byte, c character.Model, m string) (command.Executor, bool) {
 		return func(worldId byte, channelId byte, c character.Model, m string) (command.Executor, bool) {
+			var cn string
+			var itemIdStr string
+			var quantityStr string
+
+			re := regexp.MustCompile(`@award\s+(\w+)\s+item\s+(\d+)\s+(\d+)`)
+			match := re.FindStringSubmatch(m)
+			if len(match) == 4 {
+				cn = match[1]
+				itemIdStr = match[2]
+				quantityStr = match[3]
+			} else {
+				re = regexp.MustCompile(`@award\s+(\w+)\s+item\s+(\d+)`)
+				match = re.FindStringSubmatch(m)
+				if len(match) == 3 {
+					cn = match[1]
+					itemIdStr = match[2]
+					quantityStr = "1"
+				}
+			}
+
+			if len(cn) == 0 {
+				return nil, false
+			}
+
 			if !c.Gm() {
 				l.Debugf("Ignoring character [%d] command [%s], because they are not a gm.", c.Id(), m)
 				return nil, false
 			}
 
-			if !strings.HasPrefix(m, "@award item") {
+			var idProvider model.Provider[[]uint32]
+			if match[1] == "me" {
+				idProvider = model.ToSliceProvider(model.FixedProvider(c.Id()))
+			} else if match[1] == "map" {
+				idProvider = _map.CharacterIdsInMapProvider(l)(ctx)(worldId, channelId, c.MapId())
+			} else {
+				idProvider = model.ToSliceProvider(character.IdByNameProvider(l)(ctx)(match[1]))
+			}
+
+			tItemId, err := strconv.ParseUint(itemIdStr, 10, 32)
+			if err != nil {
 				return nil, false
 			}
-
-			var itemId uint32
-			var quantity uint16
-
-			re := regexp.MustCompile("@award item (\\d*) (\\d*)")
-			match := re.FindStringSubmatch(m)
-			if len(match) == 3 {
-				tItemId, err := strconv.ParseUint(match[1], 10, 32)
-				if err != nil {
-					return nil, false
-				}
-				itemId = uint32(tItemId)
-
-				tQuantity, err := strconv.ParseInt(match[2], 10, 16)
-				if err != nil {
-					return nil, false
-				}
-				quantity = uint16(tQuantity)
-			} else {
-				re = regexp.MustCompile("@award item (\\d*)")
-				match = re.FindStringSubmatch(m)
-				if len(match) == 2 {
-					tItemId, err := strconv.ParseUint(match[1], 10, 32)
-					if err != nil {
-						return nil, false
-					}
-					itemId = uint32(tItemId)
-					quantity = 1
-				} else {
-					return nil, false
-				}
-			}
-
+			itemId := uint32(tItemId)
 			exists := Exists(l)(ctx)(itemId)
 			if !exists {
 				l.Debugf("Ignoring character [%d] command [%s], because they did not input a valid item.", c.Id(), m)
 				return nil, false
 			}
 
+			tQuantity, err := strconv.ParseInt(quantityStr, 10, 16)
+			if err != nil {
+				return nil, false
+			}
+			quantity := uint16(tQuantity)
+
 			return func(l logrus.FieldLogger) func(ctx context.Context) error {
 				return func(ctx context.Context) error {
-					return GainItem(l, ctx)(c.Id(), itemId, quantity)
+					cids, err := idProvider()
+					if err != nil {
+						return err
+					}
+					for _, id := range cids {
+						_, err = CreateItem(l)(ctx)(id, itemId, quantity)
+						if err != nil {
+							l.WithError(err).Errorf("Unable to award [%d] with (%d) item [%d].", id, quantity, itemId)
+						}
+					}
+					return err
 				}
 			}, true
 		}
-	}
-}
-
-func GainItem(l logrus.FieldLogger, ctx context.Context) func(characterId uint32, itemId uint32, quantity uint16) error {
-	return func(characterId uint32, itemId uint32, quantity uint16) error {
-		_, err := CreateItem(l)(ctx)(characterId, itemId, quantity)
-		return err
 	}
 }

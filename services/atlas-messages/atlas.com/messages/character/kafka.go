4db441096152ad3441bef4cc691ea74169fdd783
Chronicle20 <a.chronicle.20@gmail.com>
2025-02-11 17:28:56 -0500

    Adjust Award Experience to command producer to match changes.

--- services/atlas-messages/atlas.com/messages/character/kafka.go
+++ services/atlas-messages/atlas.com/messages/character/kafka.go
@@ -6,6 +6,21 @@ const (
 	CommandChangeJob          = "CHANGE_JOB"
 	CommandAwardExperience    = "AWARD_EXPERIENCE"
 	CommandAwardLevel         = "AWARD_LEVEL"
+
+	ExperienceDistributionTypeWhite        = "WHITE"
+	ExperienceDistributionTypeYellow       = "YELLOW"
+	ExperienceDistributionTypeChat         = "CHAT"
+	ExperienceDistributionTypeMonsterBook  = "MONSTER_BOOK"
+	ExperienceDistributionTypeMonsterEvent = "MONSTER_EVENT"
+	ExperienceDistributionTypePlayTime     = "PLAY_TIME"
+	ExperienceDistributionTypeWedding      = "WEDDING"
+	ExperienceDistributionTypeSpiritWeek   = "SPIRIT_WEEK"
+	ExperienceDistributionTypeParty        = "PARTY"
+	ExperienceDistributionTypeItem         = "ITEM"
+	ExperienceDistributionTypeInternetCafe = "INTERNET_CAFE"
+	ExperienceDistributionTypeRainbowWeek  = "RAINBOW_WEEK"
+	ExperienceDistributionTypePartyRing    = "PARTY_RING"
+	ExperienceDistributionTypeCakePie      = "CAKE_PIE"
 )
 
 type command[E any] struct {
@@ -27,8 +42,14 @@ type changeJobCommandBody struct {
 }
 
 type awardExperienceCommandBody struct {
-	ChannelId byte   `json:"channelId"`
-	Amount    uint32 `json:"amount"`
+	ChannelId     byte                      `json:"channelId"`
+	Distributions []experienceDistributions `json:"distributions"`
+}
+
+type experienceDistributions struct {
+	ExperienceType string `json:"experienceType"`
+	Amount         uint32 `json:"amount"`
+	Attr1          uint32 `json:"attr1"`
 }
 
 type awardLevelCommandBody struct {

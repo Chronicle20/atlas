bf355d19fcddd9e1f7ffbcaffc260aba1078970b
Chronicle20 <a.chronicle.20@gmail.com>
2025-02-16 10:56:58 -0500

    Default basic attributes on character creation. This needs to be expanded for earlier version dice roll.

--- services/atlas-character-factory/atlas.com/character-factory/character/requests.go
+++ services/atlas-character-factory/atlas.com/character-factory/character/requests.go
@@ -30,20 +30,24 @@ func requestById(id uint32) requests.Request[RestModel] {
 
 func requestCreate(accountId uint32, worldId byte, name string, gender byte, mapId uint32, jobId job.Id, face uint32, hair uint32, hairColor uint32, skinColor byte) requests.Request[RestModel] {
 	i := RestModel{
-		AccountId: accountId,
-		WorldId:   worldId,
-		Name:      name,
-		Gender:    gender,
-		MapId:     mapId,
-		JobId:     uint16(jobId),
-		Face:      face,
-		Hair:      hair + hairColor,
-		SkinColor: skinColor,
-		Level:     1,
-		Hp:        50,
-		MaxHp:     50,
-		Mp:        5,
-		MaxMp:     5,
+		AccountId:    accountId,
+		WorldId:      worldId,
+		Name:         name,
+		Gender:       gender,
+		MapId:        mapId,
+		JobId:        uint16(jobId),
+		Face:         face,
+		Hair:         hair + hairColor,
+		SkinColor:    skinColor,
+		Level:        1,
+		Hp:           50,
+		MaxHp:        50,
+		Mp:           5,
+		MaxMp:        5,
+		Strength:     13,
+		Dexterity:    4,
+		Intelligence: 4,
+		Luck:         4,
 	}
 	return rest.MakePostRequest[RestModel](fmt.Sprintf(getBaseRequest()+resource), i)
 }

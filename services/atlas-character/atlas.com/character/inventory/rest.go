e289baf080a33eea802237025dac7bf3ba34c07d
Chronicle20 <a.chronicle.20@gmail.com>
2025-02-28 06:56:42 -0500

    Migrate to a maintained version of manyminds/api2go jsonapi.

--- services/atlas-character/atlas.com/character/inventory/rest.go
+++ services/atlas-character/atlas.com/character/inventory/rest.go
@@ -4,7 +4,7 @@ import (
 	"atlas-character/equipable"
 	"atlas-character/inventory/item"
 	"github.com/Chronicle20/atlas-model/model"
-	"github.com/manyminds/api2go/jsonapi"
+	"github.com/jtumidanski/api2go/jsonapi"
 )
 
 type RestModel struct {

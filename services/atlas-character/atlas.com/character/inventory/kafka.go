7e3c2f63b6d9a9ce0ae7dd76f4f4343319d5f021
Chronicle20 <chronicle20@users.noreply.github.com>
2025-03-27 16:53:30 -0400

    Supply full equipable information in inventory update.

--- services/atlas-character/atlas.com/character/inventory/kafka.go
+++ services/atlas-character/atlas.com/character/inventory/kafka.go
@@ -1,6 +1,9 @@
 package inventory
 
-import "github.com/google/uuid"
+import (
+	"github.com/google/uuid"
+	"time"
+)
 
 const (
 	EnvCommandTopic = "COMMAND_TOPIC_INVENTORY"
@@ -59,23 +62,34 @@ type inventoryChangedItemQuantityUpdateBody struct {
 }
 
 type inventoryChangedItemAttributeUpdateBody struct {
-	ItemId        uint32 `json:"itemId"`
-	Strength      uint16 `json:"strength"`
-	Dexterity     uint16 `json:"dexterity"`
-	Intelligence  uint16 `json:"intelligence"`
-	Luck          uint16 `json:"luck"`
-	HP            uint16 `json:"hp"`
-	MP            uint16 `json:"mp"`
-	WeaponAttack  uint16 `json:"weaponAttack"`
-	MagicAttack   uint16 `json:"magicAttack"`
-	WeaponDefense uint16 `json:"weaponDefense"`
-	MagicDefense  uint16 `json:"magicDefense"`
-	Accuracy      uint16 `json:"accuracy"`
-	Avoidability  uint16 `json:"avoidability"`
-	Hands         uint16 `json:"hands"`
-	Speed         uint16 `json:"speed"`
-	Jump          uint16 `json:"jump"`
-	Slots         uint16 `json:"slots"`
+	ItemId         uint32    `json:"itemId"`
+	Strength       uint16    `json:"strength"`
+	Dexterity      uint16    `json:"dexterity"`
+	Intelligence   uint16    `json:"intelligence"`
+	Luck           uint16    `json:"luck"`
+	HP             uint16    `json:"hp"`
+	MP             uint16    `json:"mp"`
+	WeaponAttack   uint16    `json:"weaponAttack"`
+	MagicAttack    uint16    `json:"magicAttack"`
+	WeaponDefense  uint16    `json:"weaponDefense"`
+	MagicDefense   uint16    `json:"magicDefense"`
+	Accuracy       uint16    `json:"accuracy"`
+	Avoidability   uint16    `json:"avoidability"`
+	Hands          uint16    `json:"hands"`
+	Speed          uint16    `json:"speed"`
+	Jump           uint16    `json:"jump"`
+	Slots          uint16    `json:"slots"`
+	OwnerName      string    `json:"ownerName"`
+	Locked         bool      `json:"locked"`
+	Spikes         bool      `json:"spikes"`
+	KarmaUsed      bool      `json:"karmaUsed"`
+	Cold           bool      `json:"cold"`
+	CanBeTraded    bool      `json:"canBeTraded"`
+	LevelType      byte      `json:"levelType"`
+	Level          byte      `json:"level"`
+	Experience     uint32    `json:"experience"`
+	HammersApplied uint32    `json:"hammersApplied"`
+	Expiration     time.Time `json:"expiration"`
 }
 
 type inventoryChangedItemMoveBody struct {

b1f1b4c3f56d544b1db998d168c0cb8850ea798b
Chronicle20 <a.chronicle.20@gmail.com>
2025-07-17 10:33:13 -0400

    Remove unused code.

--- services/atlas-character-factory/atlas.com/character-factory/kafka/consumer/character/consumer.go
+++ services/atlas-character-factory/atlas.com/character-factory/kafka/consumer/character/consumer.go
@@ -1,16 +1,15 @@
 package character
 
 import (
+	"atlas-character-factory/factory"
 	consumer2 "atlas-character-factory/kafka/consumer"
 	character3 "atlas-character-factory/kafka/message/character"
-	"atlas-character-factory/factory"
 	"atlas-character-factory/saga"
 	"context"
 
 	"github.com/Chronicle20/atlas-kafka/consumer"
 	"github.com/Chronicle20/atlas-kafka/message"
 	"github.com/Chronicle20/atlas-kafka/topic"
-	"github.com/Chronicle20/atlas-model/async"
 	"github.com/Chronicle20/atlas-model/model"
 	tenant "github.com/Chronicle20/atlas-tenant"
 	"github.com/google/uuid"
@@ -25,47 +24,6 @@ func InitConsumers(l logrus.FieldLogger) func(func(config consumer.Config, decor
 	}
 }
 
-func createdValidator(t tenant.Model) func(name string) func(l logrus.FieldLogger, ctx context.Context, event character3.StatusEvent[character3.StatusEventCreatedBody]) bool {
-	return func(name string) func(l logrus.FieldLogger, ctx context.Context, event character3.StatusEvent[character3.StatusEventCreatedBody]) bool {
-		return func(l logrus.FieldLogger, ctx context.Context, event character3.StatusEvent[character3.StatusEventCreatedBody]) bool {
-			if !t.Is(tenant.MustFromContext(ctx)) {
-				return false
-			}
-			if event.Type != character3.EventCharacterStatusTypeCreated {
-				return false
-			}
-			if name != event.Body.Name {
-				return false
-			}
-			return true
-		}
-	}
-}
-
-func createdHandler(rchan chan uint32, _ chan error) message.Handler[character3.StatusEvent[character3.StatusEventCreatedBody]] {
-	return func(l logrus.FieldLogger, ctx context.Context, m character3.StatusEvent[character3.StatusEventCreatedBody]) {
-		rchan <- m.CharacterId
-	}
-}
-
-func AwaitCreated(l logrus.FieldLogger) func(name string) async.Provider[uint32] {
-	t, _ := topic.EnvProvider(l)(character3.EnvEventTopicCharacterStatus)()
-	return func(name string) async.Provider[uint32] {
-		return func(ctx context.Context, rchan chan uint32, echan chan error) {
-			hid, err := consumer.GetManager().RegisterHandler(t, message.AdaptHandler(message.OneTimeConfig(createdValidator(tenant.MustFromContext(ctx))(name), createdHandler(rchan, echan))))
-			if err != nil {
-				echan <- err
-			}
-			l.Debugf("Creating OneTime topic consumer to await [%s] character creation. Handler [%s].", name, hid)
-			go func() {
-				<-ctx.Done()
-				l.Debugf("Removing handler [%s].", hid)
-				_ = consumer.GetManager().RemoveHandler(t, hid)
-			}()
-		}
-	}
-}
-
 // characterCreatedHandler handles character created status events to create follow-up sagas
 func characterCreatedHandler() message.Handler[character3.StatusEvent[character3.StatusEventCreatedBody]] {
 	return func(l logrus.FieldLogger, ctx context.Context, event character3.StatusEvent[character3.StatusEventCreatedBody]) {
@@ -74,7 +32,7 @@ func characterCreatedHandler() message.Handler[character3.StatusEvent[character3
 		}
 
 		t := tenant.MustFromContext(ctx)
-		
+
 		// Retrieve the stored follow-up saga template
 		template, exists := factory.GetFollowUpSagaTemplate(t.Id(), event.Body.Name)
 		if !exists {
@@ -87,8 +45,8 @@ func characterCreatedHandler() message.Handler[character3.StatusEvent[character3
 
 		// Generate a new transaction ID for the follow-up saga
 		followUpTransactionId := uuid.New()
-		
-		l.Debugf("Creating follow-up saga for character [%s] with ID [%d] and transaction [%s]", 
+
+		l.Debugf("Creating follow-up saga for character [%s] with ID [%d] and transaction [%s]",
 			event.Body.Name, event.CharacterId, followUpTransactionId.String())
 
 		// Build the follow-up saga with the actual character ID
@@ -106,12 +64,12 @@ func characterCreatedHandler() message.Handler[character3.StatusEvent[character3
 		sagaProcessor := saga.NewProcessor(l, ctx)
 		err := sagaProcessor.Create(followUpSaga)
 		if err != nil {
-			l.WithError(err).Errorf("Failed to emit follow-up saga for character [%s] with ID [%d]", 
+			l.WithError(err).Errorf("Failed to emit follow-up saga for character [%s] with ID [%d]",
 				event.Body.Name, event.CharacterId)
 			return
 		}
 
-		l.Debugf("Follow-up saga [%s] emitted successfully for character [%s] with ID [%d]", 
+		l.Debugf("Follow-up saga [%s] emitted successfully for character [%s] with ID [%d]",
 			followUpTransactionId.String(), event.Body.Name, event.CharacterId)
 	}
 }
@@ -119,7 +77,7 @@ func characterCreatedHandler() message.Handler[character3.StatusEvent[character3
 // RegisterPersistentHandlers registers persistent message handlers
 func RegisterPersistentHandlers(l logrus.FieldLogger, ctx context.Context) {
 	t, _ := topic.EnvProvider(l)(character3.EnvEventTopicCharacterStatus)()
-	
+
 	// Create a persistent handler that re-registers itself
 	var registerHandler func()
 	registerHandler = func() {
@@ -131,17 +89,17 @@ func RegisterPersistentHandlers(l logrus.FieldLogger, ctx context.Context) {
 			func(l logrus.FieldLogger, ctx context.Context, event character3.StatusEvent[character3.StatusEventCreatedBody]) {
 				// Process the event
 				characterCreatedHandler()(l, ctx, event)
-				
+
 				// Re-register the handler for the next event
 				go registerHandler()
 			})))
 		if err != nil {
 			l.WithError(err).Errorf("Failed to register character created handler")
 		}
-		
+
 		l.Debugf("Registered persistent character created handler [%s]", hid)
 	}
-	
+
 	// Start the registration
 	registerHandler()
 }
--- services/atlas-character-factory/atlas.com/character-factory/main.go
+++ services/atlas-character-factory/atlas.com/character-factory/main.go
@@ -3,9 +3,7 @@ package main
 import (
 	"atlas-character-factory/configuration"
 	"atlas-character-factory/factory"
-	"atlas-character-factory/kafka/consumer/asset"
 	"atlas-character-factory/kafka/consumer/character"
-	"atlas-character-factory/kafka/consumer/compartment"
 	"atlas-character-factory/kafka/consumer/saga"
 	"atlas-character-factory/logger"
 	"atlas-character-factory/service"
@@ -54,8 +52,6 @@ func main() {
 
 	cmf := consumer.GetManager().AddConsumer(l, tdm.Context(), tdm.WaitGroup())
 	character.InitConsumers(l)(cmf)(consumerGroupId)
-	compartment.InitConsumers(l)(cmf)(consumerGroupId)
-	asset.InitConsumers(l)(cmf)(consumerGroupId)
 	saga.InitConsumers(l)(cmf)(consumerGroupId)
 	character.RegisterPersistentHandlers(l, tdm.Context())
 	saga.InitHandlers(l)(consumer.GetManager().RegisterHandler)

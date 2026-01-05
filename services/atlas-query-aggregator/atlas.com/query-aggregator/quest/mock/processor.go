56a44f58a03be0dbbfa0df087a389d4830bfb886
Chronicle20 <a.chronicle.20@gmail.com>
2026-01-05 14:46:26 -0500

    Fix tests.

--- services/atlas-query-aggregator/atlas.com/query-aggregator/quest/mock/processor.go
+++ services/atlas-query-aggregator/atlas.com/query-aggregator/quest/mock/processor.go
@@ -8,11 +8,12 @@ import (
 
 // ProcessorImpl is a mock implementation of the quest.Processor interface
 type ProcessorImpl struct {
-	GetQuestStateFunc                func(characterId uint32, questId uint32) model.Provider[quest.State]
-	GetQuestProgressFunc             func(characterId uint32, questId uint32, infoNumber uint32) model.Provider[int]
-	GetQuestFunc                     func(characterId uint32, questId uint32) model.Provider[quest.Model]
-	GetQuestsByCharacterFunc         func(characterId uint32) model.Provider[[]quest.Model]
-	GetStartedQuestsByCharacterFunc  func(characterId uint32) model.Provider[[]quest.Model]
+	GetQuestStateFunc                 func(characterId uint32, questId uint32) model.Provider[quest.State]
+	GetQuestStatusFunc                func(characterId uint32, questId uint32) model.Provider[quest.QuestStatus]
+	GetQuestProgressFunc              func(characterId uint32, questId uint32, infoNumber uint32) model.Provider[int]
+	GetQuestFunc                      func(characterId uint32, questId uint32) model.Provider[quest.Model]
+	GetQuestsByCharacterFunc          func(characterId uint32) model.Provider[[]quest.Model]
+	GetStartedQuestsByCharacterFunc   func(characterId uint32) model.Provider[[]quest.Model]
 	GetCompletedQuestsByCharacterFunc func(characterId uint32) model.Provider[[]quest.Model]
 }
 
@@ -22,6 +23,20 @@ func (m *ProcessorImpl) GetQuestState(characterId uint32, questId uint32) model.
 		return m.GetQuestStateFunc(characterId, questId)
 	}
 	return func() (quest.State, error) {
+		return quest.StateNotStarted, nil
+	}
+}
+
+// GetQuestStatus returns the status of a quest for a character (alias for GetQuestState)
+func (m *ProcessorImpl) GetQuestStatus(characterId uint32, questId uint32) model.Provider[quest.QuestStatus] {
+	if m.GetQuestStatusFunc != nil {
+		return m.GetQuestStatusFunc(characterId, questId)
+	}
+	// Fall back to GetQuestState if GetQuestStatusFunc is not set
+	if m.GetQuestStateFunc != nil {
+		return m.GetQuestStateFunc(characterId, questId)
+	}
+	return func() (quest.QuestStatus, error) {
 		return quest.StateNotStarted, nil
 	}
 }

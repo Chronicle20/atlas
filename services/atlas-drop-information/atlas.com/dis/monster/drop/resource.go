ff8d9bc7d49e611a5efdfb003e7ea0dba80fa7fe
Chronicle20 <a.chronicle.20@gmail.com>
2026-01-07 18:22:47 -0500

    Refactor drop-information service to align with project guidelines

--- services/atlas-drop-information/atlas.com/dis/monster/drop/resource.go
+++ services/atlas-drop-information/atlas.com/dis/monster/drop/resource.go
@@ -2,6 +2,7 @@ package drop
 
 import (
 	"atlas-drops-information/rest"
+	"errors"
 	"github.com/Chronicle20/atlas-model/model"
 	"github.com/Chronicle20/atlas-rest/server"
 	"github.com/gorilla/mux"
@@ -24,8 +25,13 @@ func InitResource(si jsonapi.ServerInformation) func(db *gorm.DB) server.RouteIn
 func handleGetAllDrops(d *rest.HandlerDependency, c *rest.HandlerContext) http.HandlerFunc {
 	return rest.ParseMonsterId(d.Logger(), func(monsterId uint32) http.HandlerFunc {
 		return func(w http.ResponseWriter, r *http.Request) {
-			ms, err := GetForMonster(d.Logger())(d.Context())(d.DB())(monsterId)
+			ms, err := NewProcessor(d.Logger(), d.Context(), d.DB()).GetForMonster(monsterId)()
 			if err != nil {
+				if errors.Is(err, ErrNotFound) {
+					w.WriteHeader(http.StatusNotFound)
+					return
+				}
+				d.Logger().WithError(err).Errorf("Retrieving drops for monster [%d].", monsterId)
 				w.WriteHeader(http.StatusInternalServerError)
 				return
 			}
@@ -37,7 +43,9 @@ func handleGetAllDrops(d *rest.HandlerDependency, c *rest.HandlerContext) http.H
 				return
 			}
 
-			server.Marshal[[]RestModel](d.Logger())(w)(c.ServerInformation())(res)
+			query := r.URL.Query()
+			queryParams := jsonapi.ParseQueryFields(&query)
+			server.MarshalResponse[[]RestModel](d.Logger())(w)(c.ServerInformation())(queryParams)(res)
 		}
 	})
 }

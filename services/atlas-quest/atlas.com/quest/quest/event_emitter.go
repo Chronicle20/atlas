bb47d797a5bd64d22a14e57f415fc71d735da5ed
Chronicle20 <a.chronicle.20@gmail.com>
2026-01-24 16:06:49 -0500

    On quest completion, write item rewards to client (they are written to characters chat).

--- services/atlas-quest/atlas.com/quest/quest/event_emitter.go
+++ services/atlas-quest/atlas.com/quest/quest/event_emitter.go
@@ -1,9 +1,10 @@
 package quest
 
 import (
+	questmessage "atlas-quest/kafka/message/quest"
+	"atlas-quest/kafka/message/saga"
 	questproducer "atlas-quest/kafka/producer/quest"
 	sagaproducer "atlas-quest/kafka/producer/saga"
-	"atlas-quest/kafka/message/saga"
 	"context"
 	"time"
 
@@ -14,7 +15,7 @@ import (
 // EventEmitter defines the interface for emitting quest-related events
 type EventEmitter interface {
 	EmitQuestStarted(transactionId uuid.UUID, characterId uint32, worldId byte, questId uint32, progress string) error
-	EmitQuestCompleted(transactionId uuid.UUID, characterId uint32, worldId byte, questId uint32, completedAt time.Time) error
+	EmitQuestCompleted(transactionId uuid.UUID, characterId uint32, worldId byte, questId uint32, completedAt time.Time, items []questmessage.ItemReward) error
 	EmitQuestForfeited(transactionId uuid.UUID, characterId uint32, worldId byte, questId uint32) error
 	EmitProgressUpdated(transactionId uuid.UUID, characterId uint32, worldId byte, questId uint32, infoNumber uint32, progress string) error
 	EmitSaga(s saga.Saga) error
@@ -35,8 +36,8 @@ func (e *KafkaEventEmitter) EmitQuestStarted(transactionId uuid.UUID, characterI
 	return questproducer.EmitQuestStarted(e.l, e.ctx, transactionId, characterId, worldId, questId, progress)
 }
 
-func (e *KafkaEventEmitter) EmitQuestCompleted(transactionId uuid.UUID, characterId uint32, worldId byte, questId uint32, completedAt time.Time) error {
-	return questproducer.EmitQuestCompleted(e.l, e.ctx, transactionId, characterId, worldId, questId, completedAt)
+func (e *KafkaEventEmitter) EmitQuestCompleted(transactionId uuid.UUID, characterId uint32, worldId byte, questId uint32, completedAt time.Time, items []questmessage.ItemReward) error {
+	return questproducer.EmitQuestCompleted(e.l, e.ctx, transactionId, characterId, worldId, questId, completedAt, items)
 }
 
 func (e *KafkaEventEmitter) EmitQuestForfeited(transactionId uuid.UUID, characterId uint32, worldId byte, questId uint32) error {

4ca4a36331474b71e7ddb303d82c3749c54af3c0
Chronicle20 <a.chronicle.20@gmail.com>
2026-01-23 19:53:37 -0500

    Implement character respawn mechanism.

--- services/atlas-channel/atlas.com/channel/data/map/model.go
+++ services/atlas-channel/atlas.com/channel/data/map/model.go
@@ -1,9 +1,30 @@
 package map_
 
+import _map "github.com/Chronicle20/atlas-constants/map"
+
 type Model struct {
-	clock bool
+	clock       bool
+	returnMapId uint32
+	fieldLimit  uint32
+	town        bool
 }
 
 func (m Model) Clock() bool {
 	return m.clock
+}
+
+func (m Model) ReturnMapId() _map.Id {
+	return _map.Id(m.returnMapId)
+}
+
+func (m Model) FieldLimit() uint32 {
+	return m.fieldLimit
+}
+
+func (m Model) Town() bool {
+	return m.town
+}
+
+func (m Model) NoExpLossOnDeath() bool {
+	return _map.NoExpLossOnDeath(m.fieldLimit)
 }

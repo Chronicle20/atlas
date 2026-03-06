5a2969821129c51a611b084f9f49df0bd504ea5f
Chronicle20 <a.chronicle.20@gmail.com>
2026-03-06 09:27:57 -0500

    Consistent Avatar writing.

---
+++ services/atlas-channel/atlas.com/channel/socket/model/avatar.go
@@ -0,0 +1,125 @@
+package model
+
+import (
+	"atlas-channel/character"
+
+	"github.com/Chronicle20/atlas-constants/inventory/slot"
+	"github.com/Chronicle20/atlas-socket/response"
+	tenant "github.com/Chronicle20/atlas-tenant"
+	"github.com/sirupsen/logrus"
+)
+
+type Avatar struct {
+	gender          byte
+	skinColor       byte
+	face            uint32
+	mega            bool
+	hair            uint32
+	equipment       map[slot.Position]uint32
+	maskedEquipment map[slot.Position]uint32
+	pets            map[int8]uint32
+}
+
+func NewFromCharacter(c character.Model, mega bool) Avatar {
+	var equips = make(map[slot.Position]uint32)
+	for _, t := range slot.Slots {
+		if s, ok := c.Equipment().Get(t.Type); ok {
+			if s.CashEquipable != nil {
+				if s.Equipable != nil {
+					equips[s.Position*-1] = s.Equipable.TemplateId()
+				}
+			}
+		}
+	}
+	var maskedEquips = make(map[slot.Position]uint32)
+	for _, t := range slot.Slots {
+		if s, ok := c.Equipment().Get(t.Type); ok {
+			if s.CashEquipable != nil {
+				maskedEquips[s.Position*-1] = s.CashEquipable.TemplateId()
+				continue
+			}
+			if s.Equipable != nil {
+				maskedEquips[s.Position*-1] = s.Equipable.TemplateId()
+			}
+		}
+	}
+	var pets = make(map[int8]uint32)
+	for _, p := range c.Pets() {
+		pets[p.Slot()] = p.TemplateId()
+	}
+
+	return Avatar{
+		gender:          c.Gender(),
+		skinColor:       c.SkinColor(),
+		face:            c.Face(),
+		mega:            mega,
+		hair:            c.Hair(),
+		equipment:       equips,
+		maskedEquipment: maskedEquips,
+		pets:            pets,
+	}
+}
+
+func (m *Avatar) Encode(_ logrus.FieldLogger, t tenant.Model, _ map[string]interface{}) func(w *response.Writer) {
+	return func(w *response.Writer) {
+		if t.Region() == "GMS" && t.MajorVersion() <= 28 {
+			// older versions don't write gender / skin color / face / mega / hair a second time
+		} else {
+			w.WriteByte(m.gender)
+			w.WriteByte(m.skinColor)
+			w.WriteInt(m.face)
+			w.WriteBool(!m.mega)
+			w.WriteInt(m.hair)
+		}
+		for k, v := range m.equipment {
+			w.WriteKeyValue(byte(k), v)
+		}
+		if t.Region() == "GMS" && t.MajorVersion() <= 28 {
+			w.WriteByte(0)
+		} else {
+			w.WriteByte(0xFF)
+		}
+		for k, v := range m.maskedEquipment {
+			w.WriteKeyValue(byte(k), v)
+		}
+		if t.Region() == "GMS" && t.MajorVersion() <= 28 {
+			w.WriteByte(0)
+		} else {
+			w.WriteByte(0xFF)
+		}
+
+		//var weapon *inventory.EquippedItem
+		//for _, x := range character.Equipment() {
+		//	if x.InWeaponSlot() {
+		//		weapon = &x
+		//		break
+		//	}
+		//}
+		//if weapon != nil {
+		//	w.WriteInt(weapon.ItemId())
+		//} else {
+		w.WriteInt(0)
+		//}
+
+		// TODO confirm whether or not we should be writing TemplateId or CashId here
+		if (t.Region() == "GMS" && t.MajorVersion() > 28) || t.Region() == "JMS" {
+			for i := int8(0); i < 3; i++ {
+				if m.pets == nil {
+					w.WriteInt(0)
+					continue
+				}
+				if _, ok := m.pets[i]; ok {
+					w.WriteInt(m.pets[i])
+				} else {
+					w.WriteInt(0)
+				}
+			}
+		} else {
+			if len(m.pets) > 0 {
+				w.WriteLong(uint64(m.pets[0])) // pet cash id
+			} else {
+				w.WriteLong(0)
+			}
+		}
+	}
+}
--- services/atlas-channel/atlas.com/channel/socket/writer/character_appearance_update.go
+++ services/atlas-channel/atlas.com/channel/socket/writer/character_appearance_update.go
@@ -2,19 +2,22 @@ package writer
 
 import (
 	"atlas-channel/character"
+	"atlas-channel/socket/model"
 
 	"github.com/Chronicle20/atlas-socket/response"
 	"github.com/Chronicle20/atlas-tenant"
+	"github.com/sirupsen/logrus"
 )
 
 const CharacterAppearanceUpdate = "CharacterAppearanceUpdate"
 
-func CharacterAppearanceUpdateBody(tenant tenant.Model) func(c character.Model) BodyProducer {
+func CharacterAppearanceUpdateBody(l logrus.FieldLogger, t tenant.Model) func(c character.Model) BodyProducer {
 	return func(c character.Model) BodyProducer {
 		return func(w *response.Writer, options map[string]interface{}) []byte {
 			w.WriteInt(c.Id())
 			w.WriteByte(1) // mode, 1, 2, 4
-			WriteCharacterLook(tenant)(w, c, false)
+			ava := model.NewFromCharacter(c, false)
+			ava.Encode(l, t, options)(w)
 			w.WriteByte(0) // crush ring
 			w.WriteByte(0) // friendship ring
 			w.WriteByte(0) // marriage ring

cbf14166ff0d759cc6611bcf3a0202071ca15dad
Chronicle20 <a.chronicle.20@gmail.com>
2026-05-01 11:43:24 -0400

    feat(wz-extractor): extract Character.wz info blocks to JSON

--- services/atlas-wz-extractor/atlas.com/wz-extractor/image/character_parts.go
+++ services/atlas-wz-extractor/atlas.com/wz-extractor/image/character_parts.go
@@ -1,5 +1,13 @@
 package image
 
+import (
+	"atlas-wz-extractor/wz/property"
+	"encoding/json"
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
 // partSidecar is the JSON sidecar emitted next to each part PNG.
 type partSidecar struct {
 	Origin vec            `json:"origin"`
@@ -37,4 +45,46 @@ var stancesInScope = map[string]struct{}{
 var equipmentSubdirs = []string{
 	"Cap", "Coat", "Longcoat", "Pants", "Shoes", "Glove",
 	"Cape", "Shield", "Weapon", "Hair", "Face", "Accessory",
+}
+
+// extractInfoBlock returns a templateInfo populated from the `info` sub of
+// an equipment img. Missing fields default to zero values.
+func extractInfoBlock(props []property.Property) templateInfo {
+	info := findSub(props, "info")
+	if info == nil {
+		return templateInfo{}
+	}
+	out := templateInfo{}
+	for _, p := range info.Children() {
+		switch v := p.(type) {
+		case *property.StringProperty:
+			switch v.Name() {
+			case "islot":
+				out.Islot = v.Value()
+			case "vslot":
+				out.Vslot = v.Value()
+			}
+		case *property.IntProperty:
+			if v.Name() == "cash" {
+				out.Cash = int(v.Value())
+			}
+		case *property.ShortProperty:
+			if v.Name() == "cash" {
+				out.Cash = int(v.Value())
+			}
+		}
+	}
+	return out
+}
+
+// writeInfoJSON writes {dir}/info.json.
+func writeInfoJSON(dir string, info templateInfo) error {
+	if err := os.MkdirAll(dir, 0o755); err != nil {
+		return fmt.Errorf("mkdir %s: %w", dir, err)
+	}
+	b, err := json.MarshalIndent(info, "", "  ")
+	if err != nil {
+		return fmt.Errorf("marshal info: %w", err)
+	}
+	return os.WriteFile(filepath.Join(dir, "info.json"), b, 0o644)
 }

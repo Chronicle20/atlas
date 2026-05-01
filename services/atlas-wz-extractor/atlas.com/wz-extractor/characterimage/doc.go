b0f19284bacaf5d5a6cb185bb8ed4a920c233b3c
Chronicle20 <a.chronicle.20@gmail.com>
2026-05-01 11:48:34 -0400

    feat(characterimage): scaffold package + sentinel errors

---
+++ services/atlas-wz-extractor/atlas.com/wz-extractor/characterimage/doc.go
@@ -0,0 +1,5 @@
+// Package characterimage composes character PNGs from Character.wz extracts.
+// It is the only place in atlas-wz-extractor that knows about joint trees,
+// z-order resolution, and slot precedence; the HTTP layer is in
+// atlas-wz-extractor/characterrender.
+package characterimage

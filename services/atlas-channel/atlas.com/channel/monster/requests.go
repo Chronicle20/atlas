2a84a4d7aa11e3fc43d4414f2ed851ca088bd236
Chronicle20 <a.chronicle.20@gmail.com>
2026-05-03 17:40:07 -0400

    feat(atlas-channel): GetInMapRect client wrapper for atlas-monsters rect query

--- services/atlas-channel/atlas.com/channel/monster/requests.go
+++ services/atlas-channel/atlas.com/channel/monster/requests.go
@@ -8,8 +8,9 @@ import (
 )
 
 const (
-	mapMonstersResource = "worlds/%d/channels/%d/maps/%d/instances/%s/monsters"
-	monstersResource    = "monsters/%d"
+	mapMonstersResource     = "worlds/%d/channels/%d/maps/%d/instances/%s/monsters"
+	mapMonstersRectResource = "worlds/%d/channels/%d/maps/%d/instances/%s/monsters/in-rect?x1=%d&y1=%d&x2=%d&y2=%d&limit=%d"
+	monstersResource        = "monsters/%d"
 )
 
 func getBaseRequest() string {
@@ -18,6 +19,12 @@ func getBaseRequest() string {
 
 func requestInMap(f field.Model) requests.Request[[]RestModel] {
 	return requests.GetRequest[[]RestModel](fmt.Sprintf(getBaseRequest()+mapMonstersResource, f.WorldId(), f.ChannelId(), f.MapId(), f.Instance().String()))
+}
+
+// requestInMapRect issues the atlas-monsters rectangle query for AoE skill
+// targeting (e.g., Priest Doom). Bounds are inclusive; limit == 0 means "no cap".
+func requestInMapRect(f field.Model, x1, y1, x2, y2 int16, limit uint32) requests.Request[[]RestModel] {
+	return requests.GetRequest[[]RestModel](fmt.Sprintf(getBaseRequest()+mapMonstersRectResource, f.WorldId(), f.ChannelId(), f.MapId(), f.Instance().String(), x1, y1, x2, y2, limit))
 }
 
 func requestById(uniqueId uint32) requests.Request[RestModel] {

b59315a75ca549f4d45ee626afe3707d48bedfe0
Chronicle20 <a.chronicle.20@gmail.com>
2026-06-12 11:23:29 -0400

    feat(atlas-summons): summon status event envelope + providers

---
+++ services/atlas-summons/atlas.com/summons/summon/kafka.go
@@ -0,0 +1,46 @@
+package summon
+
+import (
+	"github.com/Chronicle20/atlas/libs/atlas-constants/channel"
+	_map "github.com/Chronicle20/atlas/libs/atlas-constants/map"
+	"github.com/Chronicle20/atlas/libs/atlas-constants/world"
+	"github.com/google/uuid"
+)
+
+const EnvEventTopicSummonStatus = "EVENT_TOPIC_SUMMON_STATUS"
+
+const (
+	EventSummonStatusCreated   = "CREATED"
+	EventSummonStatusMoved     = "MOVED"
+	EventSummonStatusAttacked  = "ATTACKED"
+	EventSummonStatusDamaged   = "DAMAGED"
+	EventSummonStatusDestroyed = "DESTROYED"
+)
+
+// StatusEvent is the summon-status event envelope. It is exported because
+// atlas-channel consumes it across the service boundary.
+type StatusEvent[E any] struct {
+	WorldId          world.Id   `json:"worldId"`
+	ChannelId        channel.Id `json:"channelId"`
+	MapId            _map.Id    `json:"mapId"`
+	Instance         uuid.UUID  `json:"instance"`
+	SummonId         uint32     `json:"summonId"`
+	OwnerCharacterId uint32     `json:"ownerCharacterId"`
+	SkillId          uint32     `json:"skillId"`
+	Type             string     `json:"type"`
+	Body             E          `json:"body"`
+}
+
+type StatusEventCreatedBody struct {
+	SkillLevel   byte  `json:"skillLevel"`
+	MovementType byte  `json:"movementType"`
+	X            int16 `json:"x"`
+	Y            int16 `json:"y"`
+	Stance       byte  `json:"stance"`
+	Puppet       bool  `json:"puppet"`
+	Animated     bool  `json:"animated"`
+}
+
+type StatusEventDestroyedBody struct {
+	Animated bool `json:"animated"`
+}
---
+++ services/atlas-summons/atlas.com/summons/summon/producer.go
@@ -0,0 +1,35 @@
+package summon
+
+import (
+	"github.com/Chronicle20/atlas/libs/atlas-kafka/producer"
+	"github.com/Chronicle20/atlas/libs/atlas-model/model"
+	"github.com/segmentio/kafka-go"
+)
+
+func createdEventProvider(m Model) model.Provider[[]kafka.Message] {
+	key := producer.CreateKey(int(m.Field().MapId()))
+	value := StatusEvent[StatusEventCreatedBody]{
+		WorldId: m.Field().WorldId(), ChannelId: m.Field().ChannelId(),
+		MapId: m.Field().MapId(), Instance: m.Field().Instance(),
+		SummonId: m.Id(), OwnerCharacterId: m.OwnerCharacterId(), SkillId: m.SkillId(),
+		Type: EventSummonStatusCreated,
+		Body: StatusEventCreatedBody{
+			SkillLevel: m.SkillLevel(), MovementType: byte(m.MovementType()),
+			X: m.X(), Y: m.Y(), Stance: m.Stance(),
+			Puppet: m.IsPuppet(), Animated: m.Animated(),
+		},
+	}
+	return producer.SingleMessageProvider(key, &value)
+}
+
+func destroyedEventProvider(m Model, animated bool) model.Provider[[]kafka.Message] {
+	key := producer.CreateKey(int(m.Field().MapId()))
+	value := StatusEvent[StatusEventDestroyedBody]{
+		WorldId: m.Field().WorldId(), ChannelId: m.Field().ChannelId(),
+		MapId: m.Field().MapId(), Instance: m.Field().Instance(),
+		SummonId: m.Id(), OwnerCharacterId: m.OwnerCharacterId(), SkillId: m.SkillId(),
+		Type: EventSummonStatusDestroyed,
+		Body: StatusEventDestroyedBody{Animated: animated},
+	}
+	return producer.SingleMessageProvider(key, &value)
+}

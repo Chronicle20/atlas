78e023b12783b63b9d4ee293b278f3c67616298f
Chronicle20 <a.chronicle.20@gmail.com>
2026-06-13 20:40:28 -0400

    feat(atlas-doors): door status event contract

---
+++ services/atlas-doors/atlas.com/doors/door/kafka.go
@@ -0,0 +1,71 @@
+package door
+
+import (
+	"github.com/Chronicle20/atlas/libs/atlas-constants/channel"
+	_map "github.com/Chronicle20/atlas/libs/atlas-constants/map"
+	"github.com/Chronicle20/atlas/libs/atlas-constants/world"
+	"github.com/google/uuid"
+)
+
+const EnvEventTopicDoorStatus = "EVENT_TOPIC_DOOR_STATUS"
+
+const (
+	EventDoorStatusCreated     = "CREATED"
+	EventDoorStatusRemoved     = "REMOVED"
+	EventDoorStatusSlotChanged = "SLOT_CHANGED"
+)
+
+// Removal reasons (FR-6.1/6.2).
+const (
+	RemoveReasonExpiry         = "EXPIRY"
+	RemoveReasonLogout         = "LOGOUT"
+	RemoveReasonChannelChanged = "CHANNEL_CHANGED"
+	RemoveReasonLeftField      = "LEFT_FIELD"
+	RemoveReasonRecast         = "RECAST"
+)
+
+type StatusEvent[E any] struct {
+	WorldId          world.Id   `json:"worldId"`
+	ChannelId        channel.Id `json:"channelId"`
+	MapId            _map.Id    `json:"mapId"` // area field map (event key)
+	Instance         uuid.UUID  `json:"instance"`
+	PairId           uint32     `json:"pairId"`
+	OwnerCharacterId uint32     `json:"ownerCharacterId"`
+	PartyId          uint32     `json:"partyId"`
+	Type             string     `json:"type"`
+	Body             E          `json:"body"`
+}
+
+type CreatedBody struct {
+	AreaDoorId   uint32  `json:"areaDoorId"`
+	TownDoorId   uint32  `json:"townDoorId"`
+	TownMapId    _map.Id `json:"townMapId"`
+	Slot         byte    `json:"slot"`
+	TownPortalId uint32  `json:"townPortalId"`
+	AreaX        int16   `json:"areaX"`
+	AreaY        int16   `json:"areaY"`
+	TownX        int16   `json:"townX"`
+	TownY        int16   `json:"townY"`
+	SkillId      uint32  `json:"skillId"`
+	SkillLevel   byte    `json:"skillLevel"`
+	ExpiresAt    int64   `json:"expiresAt"` // unix-milli
+}
+
+type RemovedBody struct {
+	AreaDoorId uint32  `json:"areaDoorId"`
+	TownDoorId uint32  `json:"townDoorId"`
+	TownMapId  _map.Id `json:"townMapId"`
+	Slot       byte    `json:"slot"`
+	Reason     string  `json:"reason"`
+}
+
+type SlotChangedBody struct {
+	AreaDoorId   uint32  `json:"areaDoorId"`
+	TownDoorId   uint32  `json:"townDoorId"`
+	TownMapId    _map.Id `json:"townMapId"`
+	OldSlot      byte    `json:"oldSlot"`
+	NewSlot      byte    `json:"newSlot"`
+	TownPortalId uint32  `json:"townPortalId"`
+	TownX        int16   `json:"townX"`
+	TownY        int16   `json:"townY"`
+}

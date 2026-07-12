f3eecc9a22fc791041cb023cff2a956d4813a9a7
Chronicle20 <a.chronicle.20@gmail.com>
2026-07-12 19:35:10 -0400

    refactor(atlas-inventory): add Processor conformance assertions to pre-existing Gen3 packages

--- services/atlas-inventory/atlas.com/inventory/inventory/processor.go
+++ services/atlas-inventory/atlas.com/inventory/inventory/processor.go
@@ -43,6 +43,8 @@ func NewProcessor(l logrus.FieldLogger, ctx context.Context, db *gorm.DB) Proces
 	return p
 }
 
+var _ Processor = (*ProcessorImpl)(nil)
+
 func (p *ProcessorImpl) WithTransaction(db *gorm.DB) Processor {
 	return &ProcessorImpl{
 		l:                    p.l,

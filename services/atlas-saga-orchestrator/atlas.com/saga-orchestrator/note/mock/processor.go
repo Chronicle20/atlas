2f7cfd1a523507592e8913415a7c85c2f389f7f8
Chronicle20 <a.chronicle.20@gmail.com>
2026-07-25 14:36:57 -0400

    feat(atlas-saga-orchestrator): note command message defs + note processor

---
+++ services/atlas-saga-orchestrator/atlas.com/saga-orchestrator/note/mock/processor.go
@@ -0,0 +1,18 @@
+package mock
+
+import (
+	"github.com/google/uuid"
+)
+
+// ProcessorMock is a mock implementation of the note.Processor interface
+type ProcessorMock struct {
+	CreateNoteFunc func(transactionId uuid.UUID, receiverId uint32, senderId uint32, message string, flag byte) error
+}
+
+// CreateNote is a mock implementation of the note.Processor.CreateNote method
+func (m *ProcessorMock) CreateNote(transactionId uuid.UUID, receiverId uint32, senderId uint32, message string, flag byte) error {
+	if m.CreateNoteFunc != nil {
+		return m.CreateNoteFunc(transactionId, receiverId, senderId, message, flag)
+	}
+	return nil
+}

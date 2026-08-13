d8ac7871bd41659ffa5fc95aefd2c146ac9ca6c0
Chronicle20 <a.chronicle.20@gmail.com>
2026-08-13 16:49:46 -0400

    feat(task-225): atlas-dragons module scaffold

---
+++ services/atlas-dragons/atlas.com/dragons/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"os"
+
+	"github.com/Chronicle20/atlas/libs/atlas-kafka/consumer"
+	"github.com/Chronicle20/atlas/libs/atlas-kafka/producer"
+	atlas "github.com/Chronicle20/atlas/libs/atlas-redis"
+	"github.com/Chronicle20/atlas/libs/atlas-rest/server"
+	service "github.com/Chronicle20/atlas/libs/atlas-service"
+)
+
+const serviceName = "atlas-dragons"
+
+type Server struct {
+	baseUrl string
+	prefix  string
+}
+
+func (s Server) GetBaseURL() string {
+	return s.baseUrl
+}
+
+func (s Server) GetPrefix() string {
+	return s.prefix
+}
+
+func GetServer() Server {
+	return Server{
+		baseUrl: "",
+		prefix:  "/api/",
+	}
+}
+
+func main() {
+	rt := service.Bootstrap(serviceName)
+	l := rt.Logger()
+
+	rc := atlas.Connect(l)
+	_ = rc
+
+	rt.TeardownFunc(func() { _ = producer.GetManager().Close(l) })
+
+	server.New(l).
+		WithContext(rt.Context()).
+		WithWaitGroup(rt.WaitGroup()).
+		SetBasePath(GetServer().GetPrefix()).
+		SetPort(os.Getenv("REST_PORT")).
+		AddRouteInitializer(server.MountHandler("/debug/consumers", consumer.GetManager().DebugHandler())).
+		AddRouteInitializer(server.MountReadiness("/readyz", rt.Ready)).
+		Run()
+
+	rt.Wait()
+}

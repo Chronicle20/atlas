08363da4778ceba0a865a3a6b75d4ac35537c3e4
Chronicle20 <a.chronicle.20@gmail.com>
2026-09-04 08:47:32 -0400

    refactor(atlas-quest): alias rest scaffolding, close db over the three input handlers

--- services/atlas-quest/atlas.com/quest/quest/resource.go
+++ services/atlas-quest/atlas.com/quest/quest/resource.go
@@ -36,7 +36,7 @@ const (
 func InitResource(si jsonapi.ServerInformation) func(db *gorm.DB) server.RouteInitializer {
 	return func(db *gorm.DB) server.RouteInitializer {
 		return func(router *mux.Router, l logrus.FieldLogger) {
-			registerGet := rest.RegisterHandler(l)(db)(si)
+			registerGet := rest.RegisterHandler(l)(si)
 
 			// Character quest routes
 			r := router.PathPrefix("/characters/{characterId}/quests").Subrouter()
@@ -45,11 +45,11 @@ func InitResource(si jsonapi.ServerInformation) func(db *gorm.DB) server.RouteIn
 			r.HandleFunc("/started", registerGet(GetStartedQuestsByCharacter, handleGetQuestsByCharacterAndState(db, StateStarted))).Methods(http.MethodGet)
 			r.HandleFunc("/completed", registerGet(GetCompletedQuestsByCharacter, handleGetQuestsByCharacterAndState(db, StateCompleted))).Methods(http.MethodGet)
 			r.HandleFunc("/{questId}", registerGet(GetQuestByCharacterAndId, handleGetQuestByCharacterAndId(db))).Methods(http.MethodGet)
-			r.HandleFunc("/{questId}/start", rest.RegisterInputHandler[StartQuestInputRestModel](l)(db)(si)(StartQuest, handleStartQuest)).Methods(http.MethodPost)
-			r.HandleFunc("/{questId}/complete", rest.RegisterInputHandler[CompleteQuestInputRestModel](l)(db)(si)(CompleteQuest, handleCompleteQuest)).Methods(http.MethodPost)
+			r.HandleFunc("/{questId}/start", rest.RegisterInputHandler[StartQuestInputRestModel](l)(si)(StartQuest, handleStartQuest(db))).Methods(http.MethodPost)
+			r.HandleFunc("/{questId}/complete", rest.RegisterInputHandler[CompleteQuestInputRestModel](l)(si)(CompleteQuest, handleCompleteQuest(db))).Methods(http.MethodPost)
 			r.HandleFunc("/{questId}/forfeit", registerGet(ForfeitQuest, handleForfeitQuest(db))).Methods(http.MethodPost)
 			r.HandleFunc("/{questId}/progress", registerGet(GetQuestProgress, handleGetQuestProgress(db))).Methods(http.MethodGet)
-			r.HandleFunc("/{questId}/progress", rest.RegisterInputHandler[progress.RestModel](l)(db)(si)(UpdateQuestProgress, handleUpdateQuestProgress)).Methods(http.MethodPatch)
+			r.HandleFunc("/{questId}/progress", rest.RegisterInputHandler[progress.RestModel](l)(si)(UpdateQuestProgress, handleUpdateQuestProgress(db))).Methods(http.MethodPatch)
 		}
 	}
 }
@@ -144,91 +144,95 @@ func handleGetQuestByCharacterAndId(db *gorm.DB) rest.GetHandler {
 	}
 }
 
-func handleStartQuest(d *rest.HandlerDependency, c *rest.HandlerContext, i StartQuestInputRestModel) http.HandlerFunc {
-	return rest.ParseCharacterId(d.Logger(), func(characterId uint32) http.HandlerFunc {
-		return rest.ParseQuestId(d.Logger(), func(questId uint32) http.HandlerFunc {
-			return func(w http.ResponseWriter, r *http.Request) {
-				f := field.NewBuilder(i.WorldId, i.ChannelId, i.MapId).Build()
-				// REST endpoints use uuid.Nil since they're not saga-initiated
-				q, failedConditions, err := NewProcessor(d.Logger(), d.Context(), d.DB()).Start(uuid.Nil, characterId, questId, f, i.SkipValidation, nil)
-				if errors.Is(err, ErrStartRequirementsNotMet) {
-					// Return 422 Unprocessable Entity with failed conditions
-					result := ValidationFailedRestModel{FailedConditions: failedConditions}
-					w.WriteHeader(http.StatusUnprocessableEntity)
-					server.MarshalResponse[ValidationFailedRestModel](d.Logger())(w)(c.ServerInformation())(r.URL.Query())(result)
-					return
-				}
-				if errors.Is(err, ErrQuestAlreadyStarted) {
-					w.WriteHeader(http.StatusConflict)
-					return
-				}
-				if errors.Is(err, ErrQuestAlreadyCompleted) {
-					w.WriteHeader(http.StatusConflict)
-					return
-				}
-				if errors.Is(err, ErrIntervalNotElapsed) {
-					w.WriteHeader(http.StatusBadRequest)
-					return
-				}
-				if err != nil {
-					d.Logger().WithError(err).Errorf("Unable to start quest [%d] for character [%d].", questId, characterId)
-					w.WriteHeader(http.StatusBadRequest)
-					return
-				}
+func handleStartQuest(db *gorm.DB) rest.InputHandler[StartQuestInputRestModel] {
+	return func(d *rest.HandlerDependency, c *rest.HandlerContext, i StartQuestInputRestModel) http.HandlerFunc {
+		return rest.ParseCharacterId(d.Logger(), func(characterId uint32) http.HandlerFunc {
+			return rest.ParseQuestId(d.Logger(), func(questId uint32) http.HandlerFunc {
+				return func(w http.ResponseWriter, r *http.Request) {
+					f := field.NewBuilder(i.WorldId, i.ChannelId, i.MapId).Build()
+					// REST endpoints use uuid.Nil since they're not saga-initiated
+					q, failedConditions, err := NewProcessor(d.Logger(), d.Context(), db).Start(uuid.Nil, characterId, questId, f, i.SkipValidation, nil)
+					if errors.Is(err, ErrStartRequirementsNotMet) {
+						// Return 422 Unprocessable Entity with failed conditions
+						result := ValidationFailedRestModel{FailedConditions: failedConditions}
+						w.WriteHeader(http.StatusUnprocessableEntity)
+						server.MarshalResponse[ValidationFailedRestModel](d.Logger())(w)(c.ServerInformation())(r.URL.Query())(result)
+						return
+					}
+					if errors.Is(err, ErrQuestAlreadyStarted) {
+						w.WriteHeader(http.StatusConflict)
+						return
+					}
+					if errors.Is(err, ErrQuestAlreadyCompleted) {
+						w.WriteHeader(http.StatusConflict)
+						return
+					}
+					if errors.Is(err, ErrIntervalNotElapsed) {
+						w.WriteHeader(http.StatusBadRequest)
+						return
+					}
+					if err != nil {
+						d.Logger().WithError(err).Errorf("Unable to start quest [%d] for character [%d].", questId, characterId)
+						w.WriteHeader(http.StatusBadRequest)
+						return
+					}
 
-				res, err := model.Map(Transform)(model.FixedProvider(q))()
-				if err != nil {
-					d.Logger().WithError(err).Errorf("Creating REST model.")
-					server.WriteErrorResponse(d.Logger())(w)(err)
-					return
-				}
+					res, err := model.Map(Transform)(model.FixedProvider(q))()
+					if err != nil {
+						d.Logger().WithError(err).Errorf("Creating REST model.")
+						server.WriteErrorResponse(d.Logger())(w)(err)
+						return
+					}
 
-				server.MarshalResponse[RestModel](d.Logger())(w)(c.ServerInformation())(r.URL.Query())(res)
-			}
+					server.MarshalResponse[RestModel](d.Logger())(w)(c.ServerInformation())(r.URL.Query())(res)
+				}
+			})
 		})
-	})
+	}
 }
 
-func handleCompleteQuest(d *rest.HandlerDependency, c *rest.HandlerContext, i CompleteQuestInputRestModel) http.HandlerFunc {
-	return rest.ParseCharacterId(d.Logger(), func(characterId uint32) http.HandlerFunc {
-		return rest.ParseQuestId(d.Logger(), func(questId uint32) http.HandlerFunc {
-			return func(w http.ResponseWriter, r *http.Request) {
-				f := field.NewBuilder(i.WorldId, i.ChannelId, i.MapId).Build()
-				// REST endpoints use uuid.Nil since they're not saga-initiated
-				nextQuestId, err := NewProcessor(d.Logger(), d.Context(), d.DB()).Complete(uuid.Nil, characterId, questId, f, i.SkipValidation, nil)
-				if errors.Is(err, gorm.ErrRecordNotFound) {
-					w.WriteHeader(http.StatusNotFound)
-					return
-				}
-				if errors.Is(err, ErrQuestExpired) {
-					w.WriteHeader(http.StatusGone) // 410 Gone for expired quests
-					return
-				}
-				if errors.Is(err, ErrQuestNotStarted) {
-					w.WriteHeader(http.StatusConflict) // 409 Conflict
-					return
-				}
-				if errors.Is(err, ErrEndRequirementsNotMet) {
-					w.WriteHeader(http.StatusUnprocessableEntity) // 422 for requirements not met
-					return
-				}
-				if err != nil {
-					d.Logger().WithError(err).Errorf("Unable to complete quest [%d] for character [%d].", questId, characterId)
-					w.WriteHeader(http.StatusBadRequest)
-					return
-				}
+func handleCompleteQuest(db *gorm.DB) rest.InputHandler[CompleteQuestInputRestModel] {
+	return func(d *rest.HandlerDependency, c *rest.HandlerContext, i CompleteQuestInputRestModel) http.HandlerFunc {
+		return rest.ParseCharacterId(d.Logger(), func(characterId uint32) http.HandlerFunc {
+			return rest.ParseQuestId(d.Logger(), func(questId uint32) http.HandlerFunc {
+				return func(w http.ResponseWriter, r *http.Request) {
+					f := field.NewBuilder(i.WorldId, i.ChannelId, i.MapId).Build()
+					// REST endpoints use uuid.Nil since they're not saga-initiated
+					nextQuestId, err := NewProcessor(d.Logger(), d.Context(), db).Complete(uuid.Nil, characterId, questId, f, i.SkipValidation, nil)
+					if errors.Is(err, gorm.ErrRecordNotFound) {
+						w.WriteHeader(http.StatusNotFound)
+						return
+					}
+					if errors.Is(err, ErrQuestExpired) {
+						w.WriteHeader(http.StatusGone) // 410 Gone for expired quests
+						return
+					}
+					if errors.Is(err, ErrQuestNotStarted) {
+						w.WriteHeader(http.StatusConflict) // 409 Conflict
+						return
+					}
+					if errors.Is(err, ErrEndRequirementsNotMet) {
+						w.WriteHeader(http.StatusUnprocessableEntity) // 422 for requirements not met
+						return
+					}
+					if err != nil {
+						d.Logger().WithError(err).Errorf("Unable to complete quest [%d] for character [%d].", questId, characterId)
+						w.WriteHeader(http.StatusBadRequest)
+						return
+					}
 
-				// Return next quest ID in response if this is part of a chain
-				if nextQuestId > 0 {
-					result := CompleteQuestResponseRestModel{NextQuestId: nextQuestId}
-					server.MarshalResponse[CompleteQuestResponseRestModel](d.Logger())(w)(c.ServerInformation())(r.URL.Query())(result)
-					return
-				}
+					// Return next quest ID in response if this is part of a chain
+					if nextQuestId > 0 {
+						result := CompleteQuestResponseRestModel{NextQuestId: nextQuestId}
+						server.MarshalResponse[CompleteQuestResponseRestModel](d.Logger())(w)(c.ServerInformation())(r.URL.Query())(result)
+						return
+					}
 
-				w.WriteHeader(http.StatusNoContent)
-			}
+					w.WriteHeader(http.StatusNoContent)
+				}
+			})
 		})
-	})
+	}
 }
 
 func handleForfeitQuest(db *gorm.DB) rest.GetHandler {
@@ -304,41 +308,43 @@ func handleGetQuestProgress(db *gorm.DB) rest.GetHandler {
 	}
 }
 
-func handleUpdateQuestProgress(d *rest.HandlerDependency, _ *rest.HandlerContext, i progress.RestModel) http.HandlerFunc {
-	return rest.ParseCharacterId(d.Logger(), func(characterId uint32) http.HandlerFunc {
-		return rest.ParseQuestId(d.Logger(), func(questId uint32) http.HandlerFunc {
-			return func(w http.ResponseWriter, r *http.Request) {
-				// Check if infoNumber is provided in path or use from body
-				infoNumberStr := mux.Vars(r)["infoNumber"]
-				var infoNumber uint32
-				if infoNumberStr != "" {
-					val, err := strconv.Atoi(infoNumberStr)
+func handleUpdateQuestProgress(db *gorm.DB) rest.InputHandler[progress.RestModel] {
+	return func(d *rest.HandlerDependency, _ *rest.HandlerContext, i progress.RestModel) http.HandlerFunc {
+		return rest.ParseCharacterId(d.Logger(), func(characterId uint32) http.HandlerFunc {
+			return rest.ParseQuestId(d.Logger(), func(questId uint32) http.HandlerFunc {
+				return func(w http.ResponseWriter, r *http.Request) {
+					// Check if infoNumber is provided in path or use from body
+					infoNumberStr := mux.Vars(r)["infoNumber"]
+					var infoNumber uint32
+					if infoNumberStr != "" {
+						val, err := strconv.Atoi(infoNumberStr)
+						if err != nil {
+							d.Logger().WithError(err).Errorf("Unable to parse infoNumber from path.")
+							w.WriteHeader(http.StatusBadRequest)
+							return
+						}
+						infoNumber = uint32(val)
+					} else {
+						infoNumber = i.InfoNumber
+					}
+
+					// REST endpoints use uuid.Nil since they're not saga-initiated
+					err := NewProcessor(d.Logger(), d.Context(), db).SetProgress(uuid.Nil, characterId, questId, infoNumber, i.Progress)
+					if errors.Is(err, gorm.ErrRecordNotFound) {
+						w.WriteHeader(http.StatusNotFound)
+						return
+					}
 					if err != nil {
-						d.Logger().WithError(err).Errorf("Unable to parse infoNumber from path.")
+						d.Logger().WithError(err).Errorf("Unable to update progress for quest [%d] for character [%d].", questId, characterId)
 						w.WriteHeader(http.StatusBadRequest)
 						return
 					}
-					infoNumber = uint32(val)
-				} else {
-					infoNumber = i.InfoNumber
-				}
 
-				// REST endpoints use uuid.Nil since they're not saga-initiated
-				err := NewProcessor(d.Logger(), d.Context(), d.DB()).SetProgress(uuid.Nil, characterId, questId, infoNumber, i.Progress)
-				if errors.Is(err, gorm.ErrRecordNotFound) {
-					w.WriteHeader(http.StatusNotFound)
-					return
+					w.WriteHeader(http.StatusNoContent)
 				}
-				if err != nil {
-					d.Logger().WithError(err).Errorf("Unable to update progress for quest [%d] for character [%d].", questId, characterId)
-					w.WriteHeader(http.StatusBadRequest)
-					return
-				}
-
-				w.WriteHeader(http.StatusNoContent)
-			}
+			})
 		})
-	})
+	}
 }
 
 func handleDeleteQuestsByCharacter(db *gorm.DB) rest.GetHandler {

4db1055c77a10ec0a43352f14312d0634fbd389c
Chronicle20 <a.chronicle.20@gmail.com>
2026-09-04 10:29:40 -0400

    refactor(atlas-ban): delegate id parsers to shared server parsers

--- services/atlas-ban/atlas.com/ban/rest/handler.go
+++ services/atlas-ban/atlas.com/ban/rest/handler.go
@@ -2,10 +2,8 @@ package rest
 
 import (
 	"net/http"
-	"strconv"
 
 	"github.com/google/uuid"
-	"github.com/gorilla/mux"
 	"github.com/jtumidanski/api2go/jsonapi"
 	"github.com/sirupsen/logrus"
 
@@ -26,47 +24,14 @@ func RegisterInputHandler[M any](l logrus.FieldLogger) func(si jsonapi.ServerInf
 	return server.RegisterInputHandler[M](l)
 }
 
-type BanIdHandler func(id uint32) http.HandlerFunc
-
-func ParseBanId(l logrus.FieldLogger, next BanIdHandler) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		vars := mux.Vars(r)
-		value, err := strconv.Atoi(vars["banId"])
-		if err != nil {
-			l.WithError(err).Errorln("Error parsing id as uint32")
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-		next(uint32(value))(w, r)
-	}
+func ParseBanId(l logrus.FieldLogger, next func(uint32) http.HandlerFunc) http.HandlerFunc {
+	return server.ParseIntId[uint32](l, "banId", next)
 }
 
-type AccountIdHandler func(id uint32) http.HandlerFunc
-
-func ParseAccountId(l logrus.FieldLogger, next AccountIdHandler) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		vars := mux.Vars(r)
-		value, err := strconv.Atoi(vars["accountId"])
-		if err != nil {
-			l.WithError(err).Errorln("Error parsing id as uint32")
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-		next(uint32(value))(w, r)
-	}
+func ParseAccountId(l logrus.FieldLogger, next func(uint32) http.HandlerFunc) http.HandlerFunc {
+	return server.ParseIntId[uint32](l, "accountId", next)
 }
 
-type ReportIdHandler func(id uuid.UUID) http.HandlerFunc
-
-func ParseReportId(l logrus.FieldLogger, next ReportIdHandler) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		vars := mux.Vars(r)
-		id, err := uuid.Parse(vars["reportId"])
-		if err != nil {
-			l.WithError(err).Errorln("Error parsing reportId as uuid")
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-		next(id)(w, r)
-	}
+func ParseReportId(l logrus.FieldLogger, next func(uuid.UUID) http.HandlerFunc) http.HandlerFunc {
+	return server.ParseUUIDId(l, "reportId", next)
 }

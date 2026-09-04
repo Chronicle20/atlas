f3a14e69c96a0de9a83813d5ab8093b8c56599bd
Chronicle20 <a.chronicle.20@gmail.com>
2026-09-04 08:38:59 -0400

    refactor(atlas-drop-information): delegate id parsers to server.ParseIntId

--- services/atlas-drop-information/atlas.com/dis/rest/handler.go
+++ services/atlas-drop-information/atlas.com/dis/rest/handler.go
@@ -2,9 +2,7 @@ package rest
 
 import (
 	"net/http"
-	"strconv"
 
-	"github.com/gorilla/mux"
 	"github.com/sirupsen/logrus"
 
 	"github.com/Chronicle20/atlas/libs/atlas-rest/server"
@@ -18,30 +16,10 @@ type GetHandler = server.GetHandler
 
 var RegisterHandler = server.RegisterHandler
 
-type MonsterIdHandler func(monsterId uint32) http.HandlerFunc
-
-func ParseMonsterId(l logrus.FieldLogger, next MonsterIdHandler) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		monsterId, err := strconv.Atoi(mux.Vars(r)["monsterId"])
-		if err != nil {
-			l.WithError(err).Errorf("Unable to properly parse monsterId from path.")
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-		next(uint32(monsterId))(w, r)
-	}
+func ParseMonsterId(l logrus.FieldLogger, next func(uint32) http.HandlerFunc) http.HandlerFunc {
+	return server.ParseIntId[uint32](l, "monsterId", next)
 }
 
-type ItemIdHandler func(itemId uint32) http.HandlerFunc
-
-func ParseItemId(l logrus.FieldLogger, next ItemIdHandler) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		itemId, err := strconv.Atoi(mux.Vars(r)["itemId"])
-		if err != nil {
-			l.WithError(err).Errorf("Unable to properly parse itemId from path.")
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-		next(uint32(itemId))(w, r)
-	}
+func ParseItemId(l logrus.FieldLogger, next func(uint32) http.HandlerFunc) http.HandlerFunc {
+	return server.ParseIntId[uint32](l, "itemId", next)
 }

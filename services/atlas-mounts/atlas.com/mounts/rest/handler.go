dabb2eabefb9551b6c4af9efb189e43561ff31cc
Chronicle20 <a.chronicle.20@gmail.com>
2026-09-04 08:28:39 -0400

    refactor(atlas-mounts): alias rest scaffolding, close db over handler constructor

--- services/atlas-mounts/atlas.com/mounts/rest/handler.go
+++ services/atlas-mounts/atlas.com/mounts/rest/handler.go
@@ -1,60 +1,22 @@
 package rest
 
 import (
-	"context"
 	"net/http"
 	"strconv"
 
 	"github.com/gorilla/mux"
-	"github.com/jtumidanski/api2go/jsonapi"
 	"github.com/sirupsen/logrus"
-	"gorm.io/gorm"
 
 	"github.com/Chronicle20/atlas/libs/atlas-rest/server"
 )
 
-type HandlerDependency struct {
-	l   logrus.FieldLogger
-	db  *gorm.DB
-	ctx context.Context
-}
+type HandlerDependency = server.HandlerDependency
 
-func (h HandlerDependency) Logger() logrus.FieldLogger {
-	return h.l
-}
+type HandlerContext = server.HandlerContext
 
-func (h HandlerDependency) DB() *gorm.DB {
-	return h.db
-}
+type GetHandler = server.GetHandler
 
-func (h HandlerDependency) Context() context.Context {
-	return h.ctx
-}
-
-type HandlerContext struct {
-	si jsonapi.ServerInformation
-}
-
-func (h HandlerContext) ServerInformation() jsonapi.ServerInformation {
-	return h.si
-}
-
-type GetHandler func(d *HandlerDependency, c *HandlerContext) http.HandlerFunc
-
-func RegisterHandler(l logrus.FieldLogger) func(db *gorm.DB) func(si jsonapi.ServerInformation) func(handlerName string, handler GetHandler) http.HandlerFunc {
-	return func(db *gorm.DB) func(si jsonapi.ServerInformation) func(handlerName string, handler GetHandler) http.HandlerFunc {
-		return func(si jsonapi.ServerInformation) func(handlerName string, handler GetHandler) http.HandlerFunc {
-			return func(handlerName string, handler GetHandler) http.HandlerFunc {
-				return server.RetrieveSpan(l, handlerName, context.Background(), func(sl logrus.FieldLogger, sctx context.Context) http.HandlerFunc {
-					fl := sl.WithFields(logrus.Fields{"originator": handlerName, "type": "rest_handler"})
-					return server.ParseTenant(fl, sctx, func(tl logrus.FieldLogger, tctx context.Context) http.HandlerFunc {
-						return handler(&HandlerDependency{l: tl, db: db, ctx: tctx}, &HandlerContext{si: si})
-					})
-				})
-			}
-		}
-	}
-}
+var RegisterHandler = server.RegisterHandler
 
 type CharacterIdHandler func(characterId uint32) http.HandlerFunc
 

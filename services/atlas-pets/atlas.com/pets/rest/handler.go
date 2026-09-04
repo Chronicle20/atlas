3ae29d51997f4006bcbb18a01ce801bdee2cc091
Chronicle20 <a.chronicle.20@gmail.com>
2026-09-04 08:41:45 -0400

    refactor(atlas-pets): delegate id parsers to server.ParseIntId

--- services/atlas-pets/atlas.com/pets/rest/handler.go
+++ services/atlas-pets/atlas.com/pets/rest/handler.go
@@ -2,9 +2,7 @@ package rest
 
 import (
 	"net/http"
-	"strconv"
 
-	"github.com/gorilla/mux"
 	"github.com/jtumidanski/api2go/jsonapi"
 	"github.com/sirupsen/logrus"
 
@@ -25,31 +23,10 @@ func RegisterInputHandler[M any](l logrus.FieldLogger) func(si jsonapi.ServerInf
 	return server.RegisterInputHandler[M](l)
 }
 
-type CharacterIdHandler func(characterId uint32) http.HandlerFunc
-
-func ParseCharacterId(l logrus.FieldLogger, next CharacterIdHandler) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		characterId, err := strconv.Atoi(mux.Vars(r)["characterId"])
-		if err != nil {
-			l.WithError(err).Errorf("Unable to properly parse characterId from path.")
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-		next(uint32(characterId))(w, r)
-	}
+func ParseCharacterId(l logrus.FieldLogger, next func(uint32) http.HandlerFunc) http.HandlerFunc {
+	return server.ParseIntId[uint32](l, "characterId", next)
 }
 
-type PetIdHandler func(petId uint32) http.HandlerFunc
-
-func ParsePetId(l logrus.FieldLogger, next PetIdHandler) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		vars := mux.Vars(r)
-		petId, err := strconv.Atoi(vars["petId"])
-		if err != nil {
-			l.WithError(err).Errorf("Error parsing petId as uint32")
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-		next(uint32(petId))(w, r)
-	}
+func ParsePetId(l logrus.FieldLogger, next func(uint32) http.HandlerFunc) http.HandlerFunc {
+	return server.ParseIntId[uint32](l, "petId", next)
 }

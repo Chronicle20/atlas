0fd55ce60dd692814a575f00d462263c85672a7f
Chronicle20 <a.chronicle.20@gmail.com>
2026-09-04 11:37:11 -0400

    refactor(atlas-mts): delegate id parsers to shared server parsers

--- services/atlas-mts/atlas.com/mts/rest/handler.go
+++ services/atlas-mts/atlas.com/mts/rest/handler.go
@@ -2,9 +2,7 @@ package rest
 
 import (
 	"net/http"
-	"strconv"
 
-	"github.com/gorilla/mux"
 	"github.com/jtumidanski/api2go/jsonapi"
 	"github.com/sirupsen/logrus"
 
@@ -28,83 +26,25 @@ func RegisterInputHandler[M any](l logrus.FieldLogger) func(si jsonapi.ServerInf
 
 // ParseWorldId parses the {worldId} path var into a world.Id (a byte).
 func ParseWorldId(l logrus.FieldLogger, next func(worldId world.Id) http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		worldIdStr, ok := mux.Vars(r)["worldId"]
-		if !ok {
-			l.Errorf("Unable to properly parse worldId from path.")
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-		worldId, err := strconv.ParseUint(worldIdStr, 10, 8)
-		if err != nil {
-			l.WithError(err).Errorf("Unable to properly parse worldId from path.")
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-		next(world.Id(byte(worldId)))(w, r)
-	}
+	return server.ParseIntId[world.Id](l, "worldId", next)
 }
 
 // ParseCharacterId parses the {characterId} path var into a uint32.
 func ParseCharacterId(l logrus.FieldLogger, next func(characterId uint32) http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		characterIdStr, ok := mux.Vars(r)["characterId"]
-		if !ok {
-			l.Errorf("Unable to properly parse characterId from path.")
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-		characterId, err := strconv.ParseUint(characterIdStr, 10, 32)
-		if err != nil {
-			l.WithError(err).Errorf("Unable to properly parse characterId from path.")
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-		next(uint32(characterId))(w, r)
-	}
+	return server.ParseIntId[uint32](l, "characterId", next)
 }
 
 // ParseAccountId parses the {accountId} path var into a uint32.
 func ParseAccountId(l logrus.FieldLogger, next func(accountId uint32) http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		accountIdStr, ok := mux.Vars(r)["accountId"]
-		if !ok {
-			l.Errorf("Unable to properly parse accountId from path.")
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-		accountId, err := strconv.ParseUint(accountIdStr, 10, 32)
-		if err != nil {
-			l.WithError(err).Errorf("Unable to properly parse accountId from path.")
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-		next(uint32(accountId))(w, r)
-	}
+	return server.ParseIntId[uint32](l, "accountId", next)
 }
 
 // ParseListingId parses the {listingId} path var (a UUID string).
 func ParseListingId(l logrus.FieldLogger, next func(listingId string) http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		listingId, ok := mux.Vars(r)["listingId"]
-		if !ok || listingId == "" {
-			l.Errorf("Unable to properly parse listingId from path.")
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-		next(listingId)(w, r)
-	}
+	return server.ParseStringId(l, "listingId", next)
 }
 
 // ParseHoldingId parses the {holdingId} path var (a UUID string).
 func ParseHoldingId(l logrus.FieldLogger, next func(holdingId string) http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		holdingId, ok := mux.Vars(r)["holdingId"]
-		if !ok || holdingId == "" {
-			l.Errorf("Unable to properly parse holdingId from path.")
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-		next(holdingId)(w, r)
-	}
+	return server.ParseStringId(l, "holdingId", next)
 }

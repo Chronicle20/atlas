b0834c97f3a9fdcc537baaaafa25eb51f0dae0c1
Chronicle20 <a.chronicle.20@gmail.com>
2026-09-04 09:10:23 -0400

    refactor(atlas-portal-actions): delegate id parsers to shared server parsers

--- services/atlas-portal-actions/atlas.com/portal/rest/handler.go
+++ services/atlas-portal-actions/atlas.com/portal/rest/handler.go
@@ -4,7 +4,6 @@ import (
 	"net/http"
 
 	"github.com/google/uuid"
-	"github.com/gorilla/mux"
 	"github.com/jtumidanski/api2go/jsonapi"
 	"github.com/sirupsen/logrus"
 
@@ -25,31 +24,10 @@ func RegisterInputHandler[M any](l logrus.FieldLogger) func(si jsonapi.ServerInf
 	return server.RegisterInputHandler[M](l)
 }
 
-type ScriptIdHandler func(scriptId uuid.UUID) http.HandlerFunc
-
-func ParseScriptId(l logrus.FieldLogger, next ScriptIdHandler) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		scriptIdStr := mux.Vars(r)["scriptId"]
-		scriptId, err := uuid.Parse(scriptIdStr)
-		if err != nil {
-			l.WithError(err).Errorf("Unable to properly parse scriptId from path.")
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-		next(scriptId)(w, r)
-	}
+func ParseScriptId(l logrus.FieldLogger, next func(scriptId uuid.UUID) http.HandlerFunc) http.HandlerFunc {
+	return server.ParseUUIDId(l, "scriptId", next)
 }
 
-type PortalIdHandler func(portalId string) http.HandlerFunc
-
-func ParsePortalId(l logrus.FieldLogger, next PortalIdHandler) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		portalId := mux.Vars(r)["portalId"]
-		if portalId == "" {
-			l.Errorf("Portal ID is required")
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-		next(portalId)(w, r)
-	}
+func ParsePortalId(l logrus.FieldLogger, next func(portalId string) http.HandlerFunc) http.HandlerFunc {
+	return server.ParseStringId(l, "portalId", next)
 }
